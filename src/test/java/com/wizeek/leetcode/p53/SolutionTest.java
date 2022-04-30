f1db6b0b07bfa543b8da0d8ff032d9e92984b0a2
Artur <wizeek80@gmail.com>
2022-04-29 22:47:55 -0700

    p53 brute force triple loop

---
+++ src/test/java/com/wizeek/leetcode/p53/SolutionTest.java
@@ -0,0 +1,36 @@
+package com.wizeek.leetcode.p53;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class SolutionTest {
+
+    private Solution solution;
+
+    @Before
+    public void setUp() {
+        solution = new Solution();
+    }
+
+    @Test
+    public void test1() {
+        assertEquals(6, solution.maxSubArray(new int[]{-2, 1, -3, 4, -1, 2, 1, -5, 4}));
+    }
+
+    @Test
+    public void test2() {
+        assertEquals(1, solution.maxSubArray(new int[]{1}));
+    }
+
+    @Test
+    public void test3() {
+        assertEquals(23, solution.maxSubArray(new int[]{5, 4, -1, 7, 8}));
+    }
+
+    @Test
+    public void test4() {
+        assertEquals(-1, solution.maxSubArray(new int[]{-1}));
+    }
+}
