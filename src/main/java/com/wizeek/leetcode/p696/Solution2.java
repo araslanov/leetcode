a037e25b2d0d23208b58ef6413c06a23223e0449
Artur <wizeek80@gmail.com>
2022-05-02 19:55:41 -0700

    p696 optimized second solution without list

--- src/main/java/com/wizeek/leetcode/p696/Solution2.java
+++ src/main/java/com/wizeek/leetcode/p696/Solution2.java
@@ -1,8 +1,5 @@
 package com.wizeek.leetcode.p696;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class Solution2 {
     public int countBinarySubstrings(String s) {
         if (s.length() == 1) {
@@ -10,13 +7,17 @@ public class Solution2 {
         }
         boolean isPreviousZero = s.charAt(0) == '0';
         int consecutiveDigits = 1;
-        List<Integer> consecutiveDigitList = new ArrayList<>(s.length());
+        int previousConsecutiveDigits = 0;
+        int result = 0;
         for (int i = 1; i < s.length(); i++) {
             if (s.charAt(i) == '0') {
                 if (isPreviousZero) {
                     consecutiveDigits++;
                 } else {
-                    consecutiveDigitList.add(consecutiveDigits);
+                    if (previousConsecutiveDigits > 0) {
+                        result += Math.min(previousConsecutiveDigits, consecutiveDigits);
+                    }
+                    previousConsecutiveDigits = consecutiveDigits;
                     consecutiveDigits = 1;
                 }
                 isPreviousZero = true;
@@ -24,19 +25,16 @@ public class Solution2 {
                 if (!isPreviousZero) {
                     consecutiveDigits++;
                 } else {
-                    consecutiveDigitList.add(consecutiveDigits);
+                    if (previousConsecutiveDigits > 0) {
+                        result += Math.min(previousConsecutiveDigits, consecutiveDigits);
+                    }
+                    previousConsecutiveDigits = consecutiveDigits;
                     consecutiveDigits = 1;
                 }
                 isPreviousZero = false;
             }
         }
-        consecutiveDigitList.add(consecutiveDigits);
-        int result = 0;
-        for (int i = 0; i < consecutiveDigitList.size() - 1; i++) {
-            int left = consecutiveDigitList.get(i);
-            int right = consecutiveDigitList.get(i + 1);
-            result += Math.min(left, right);
-        }
+        result += Math.min(previousConsecutiveDigits, consecutiveDigits);
         return result;
     }
 }
