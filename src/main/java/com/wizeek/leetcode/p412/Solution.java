6ccebc2006039bd520e327b302bf723569a27eb8
Artur <wizeek80@gmail.com>
2022-05-04 20:22:07 -0700

    p412 improved solution O(n/3)

--- src/main/java/com/wizeek/leetcode/p412/Solution.java
+++ src/main/java/com/wizeek/leetcode/p412/Solution.java
@@ -1,23 +1,29 @@
 package com.wizeek.leetcode.p412;
 
-import java.util.LinkedList;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Solution {
     public List<String> fizzBuzz(int n) {
-        List<String> result = new LinkedList<>();
-        for (int i = 1; i <= n; i++) {
-            String s = "";
-            if (i % 3 == 0) {
-                s += "Fizz";
+        List<String> result = new ArrayList<>(n);
+        int i = 3;
+        while (i <= n) {
+            result.add(String.valueOf(i - 2));
+            result.add(String.valueOf(i - 1));
+            result.add("Fizz");
+            i += 3;
+        }
+        for (int j = 1; j <= n % 3; j++) {
+            result.add(String.valueOf(i - 3 + j));
+        }
+        i = 5;
+        while (i <= n) {
+            if (i % 15 == 0) {
+                result.set(i - 1, "FizzBuzz");
+            } else {
+                result.set(i - 1, "Buzz");
             }
-            if (i % 5 == 0) {
-                s += "Buzz";
-            }
-            if ("".equals(s)) {
-                s = String.valueOf(i);
-            }
-            result.add(s);
+            i += 5;
         }
         return result;
     }
