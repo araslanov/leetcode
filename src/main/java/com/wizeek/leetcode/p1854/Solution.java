ad96b8f13978ba9f36fb00533682bddff42f280c
Artur <wizeek80@gmail.com>
2022-05-10 00:08:11 -0700

    p1584 with int array instead of map

--- src/main/java/com/wizeek/leetcode/p1854/Solution.java
+++ src/main/java/com/wizeek/leetcode/p1854/Solution.java
@@ -1,17 +1,13 @@
 package com.wizeek.leetcode.p1854;
 
-import java.util.HashMap;
-import java.util.Map;
-
 public class Solution {
     public int maximumPopulation(int[][] logs) {
-        Map<Integer, Integer> populationByYear = new HashMap<>();
+        int[] populationByYear = new int[101];
         int maxPop = 0;
         int firstYearOfMaxPop = 0;
         for (int[] log : logs) {
             for (int year = log[0]; year < log[1]; year++) {
-                populationByYear.merge(year, 1, Integer::sum);
-                int pop = populationByYear.get(year);
+                int pop = ++populationByYear[year - 1950];
                 if (maxPop < pop) {
                     firstYearOfMaxPop = year;
                     maxPop = pop;
