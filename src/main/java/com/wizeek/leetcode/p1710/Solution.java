deeba3fa15108bf3539e0794e7724b275d174d2b
Artur <wizeek80@gmail.com>
2022-05-10 23:56:59 -0700

    p1710 optimized with max capacity

--- src/main/java/com/wizeek/leetcode/p1710/Solution.java
+++ src/main/java/com/wizeek/leetcode/p1710/Solution.java
@@ -4,10 +4,14 @@ public class Solution {
     public int maximumUnits(int[][] boxTypes, int truckSize) {
         int[] boxes = new int[1000];
         int result = 0;
+        int maxCapacity = 0;
         for (int[] boxType : boxTypes) {
             boxes[boxType[1] - 1] += boxType[0];
+            if (maxCapacity < boxType[1]) {
+                maxCapacity = boxType[1];
+            }
         }
-        for (int i = 999; i >= 0; i--) {
+        for (int i = maxCapacity - 1; i >= 0; i--) {
             int maxOfThisType = Math.min(truckSize, boxes[i]);
             result += maxOfThisType * (i + 1);
             truckSize -= maxOfThisType;
