be065f70795b9e950b8b3d107c60cc3959d3e273
Artur <wizeek80@gmail.com>
2022-05-12 00:11:38 -0700

    p387 optimized with array instead of map

--- src/main/java/com/wizeek/leetcode/p387/Solution.java
+++ src/main/java/com/wizeek/leetcode/p387/Solution.java
@@ -1,26 +1,15 @@
 package com.wizeek.leetcode.p387;
 
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
-
 public class Solution {
     public int firstUniqChar(String s) {
-        Set<Character> repeatedChars = new HashSet<>();
-        Map<Character, Integer> frequencyMap = new HashMap<>();
+        int[] frequency = new int[26];
         for (char c : s.toCharArray()) {
-            if (repeatedChars.contains(c)) {
-                continue;
-            }
-            int newReps = frequencyMap.getOrDefault(c, 0) + 1;
-            frequencyMap.put(c, newReps);
-            if (newReps == 2) {
-                repeatedChars.add(c);
-            }
+            int i = c - 'a';
+            frequency[i]++;
         }
         for (int i = 0; i < s.length(); i++) {
-            if (frequencyMap.get(s.charAt(i)) == 1) {
+            int j = s.charAt(i) - 'a';
+            if (frequency[j] == 1) {
                 return i;
             }
         }
