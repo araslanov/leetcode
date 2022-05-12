f76a9bb64f11ca81f3af21817fbc767139356a7c
Artur <wizeek80@gmail.com>
2022-05-11 20:36:25 -0700

    p387 slightly optimized with duplicate hashset

--- src/main/java/com/wizeek/leetcode/p387/Solution.java
+++ src/main/java/com/wizeek/leetcode/p387/Solution.java
@@ -1,13 +1,23 @@
 package com.wizeek.leetcode.p387;
 
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Set;
 
 public class Solution {
     public int firstUniqChar(String s) {
+        Set<Character> repeatedChars = new HashSet<>();
         Map<Character, Integer> frequencyMap = new HashMap<>();
         for (char c : s.toCharArray()) {
-            frequencyMap.put(c, frequencyMap.getOrDefault(c, 0) + 1);
+            if (repeatedChars.contains(c)) {
+                continue;
+            }
+            int newReps = frequencyMap.getOrDefault(c, 0) + 1;
+            frequencyMap.put(c, newReps);
+            if (newReps == 2) {
+                repeatedChars.add(c);
+            }
         }
         for (int i = 0; i < s.length(); i++) {
             if (frequencyMap.get(s.charAt(i)) == 1) {
