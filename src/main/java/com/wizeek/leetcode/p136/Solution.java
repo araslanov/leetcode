07b54ee72de9f5bf15450f331d9f710b84368416
Artur <wizeek80@gmail.com>
2022-05-16 12:06:22 -0700

    p136 with bitwise xor

--- src/main/java/com/wizeek/leetcode/p136/Solution.java
+++ src/main/java/com/wizeek/leetcode/p136/Solution.java
@@ -1,19 +1,11 @@
 package com.wizeek.leetcode.p136;
 
-import java.util.HashMap;
-import java.util.Map;
-
 public class Solution {
     public int singleNumber(int[] nums) {
-        Map<Integer, Integer> map = new HashMap<>();
+        int result = 0;
         for (int num : nums) {
-            map.put(num, map.getOrDefault(num, 0) + 1);
+            result ^= num;
         }
-        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
-            if (entry.getValue() == 1) {
-                return entry.getKey();
-            }
-        }
-        return 0;
+        return result;
     }
 }
