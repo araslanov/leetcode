abfd275127cc4e863e20255e9eb0769f10e6919c
Artur <wizeek80@gmail.com>
2022-05-16 10:21:18 -0700

    p1406 with array instead of map

--- src/main/java/com/wizeek/leetcode/p1046/Solution.java
+++ src/main/java/com/wizeek/leetcode/p1046/Solution.java
@@ -1,43 +1,40 @@
 package com.wizeek.leetcode.p1046;
 
-import java.util.Map;
-import java.util.TreeMap;
-
 public class Solution {
     public int lastStoneWeight(int[] stones) {
-        if (stones.length < 2) {
-            return stones[0];
-        }
-        TreeMap<Integer, Integer> stoneToCount = new TreeMap<>();
+        int[] weights = new int[1000];
         for (int stone : stones) {
-            stoneToCount.put(stone, stoneToCount.getOrDefault(stone, 0) + 1);
+            weights[stone - 1]++;
         }
-        Integer smaller;
-        Integer larger;
-        while (stoneToCount.size() > 1) {
-            Map.Entry<Integer, Integer> lastEntry = stoneToCount.pollLastEntry();
-            larger = lastEntry.getKey();
-            if (lastEntry.getValue() == 1) {
-                Map.Entry<Integer, Integer> secondToLastEntry = stoneToCount.pollLastEntry();
-                smaller = secondToLastEntry.getKey();
-                if (secondToLastEntry.getValue() > 1) {
-                    stoneToCount.put(secondToLastEntry.getKey(), secondToLastEntry.getValue() - 1);
-                }
-            } else if (lastEntry.getValue() == 2) {
-                smaller = larger;
+        int larger = 0;
+        int smaller = 0;
+        int i = 999;
+        int j = 0;
+        while (i >= 0) {
+            if (weights[i] == 0) {
+                i--;
             } else {
-                smaller = larger;
-                stoneToCount.put(lastEntry.getKey(), lastEntry.getValue() - 2);
-            }
-            int diff = larger - smaller;
-            if (diff > 0) {
-                stoneToCount.put(diff, stoneToCount.getOrDefault(diff, 0) + 1);
+                larger = i + 1;
+                weights[i]--;
+                j = i;
+                while (j >= 0) {
+                    if (weights[j] == 0) {
+                        j--;
+                    } else {
+                        smaller = j + 1;
+                        weights[j]--;
+                        break;
+                    }
+                }
+                if (j < 0) {
+                    return i + 1;
+                }
+                int diff = larger - smaller;
+                if (diff > 0) {
+                    weights[diff - 1]++;
+                }
             }
         }
-        if (stoneToCount.size() == 0) {
-            return 0;
-        }
-        Map.Entry<Integer, Integer> lastEntry = stoneToCount.pollLastEntry();
-        return lastEntry.getValue() % 2 == 0 ? 0 : lastEntry.getKey();
+        return 0;
     }
 }
