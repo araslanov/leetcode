5d19fdda1e8462b662b16ef13ba7da34feec44a9
Artur <wizeek80@gmail.com>
2022-05-17 15:11:26 -0700

    1260 optimized O(mn) instead of O(2mn)

--- src/main/java/com/wizeek/leetcode/p1260/Solution.java
+++ src/main/java/com/wizeek/leetcode/p1260/Solution.java
@@ -1,45 +1,21 @@
 package com.wizeek.leetcode.p1260;
 
 import java.util.ArrayList;
-import java.util.LinkedList;
 import java.util.List;
-import java.util.Queue;
 
 public class Solution {
     public List<List<Integer>> shiftGrid(int[][] grid, int k) {
-        if (k == 0) {
-            return convertArrayToList(grid);
-        }
-        Queue<Integer> buffer = new LinkedList<>();
-        int m = grid.length;
-        int n = grid[0].length;
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                int currentValue = grid[i][j];
-                int nextLinearIndex = (i * n + j + k) % (m * n);
-                int newI = nextLinearIndex / n;
-                int newJ = nextLinearIndex % n;
-                if (buffer.size() == k % (m * n)) {
-                    buffer.offer(grid[newI][newJ]);
-                    int newValue = buffer.poll();
-                    grid[newI][newJ] = newValue;
-                } else {
-                    buffer.offer(grid[newI][newJ]);
-                    grid[newI][newJ] = currentValue;
-                }
-            }
-        }
-        return convertArrayToList(grid);
-    }
-
-    private static List<List<Integer>> convertArrayToList(int[][] grid) {
         int m = grid.length;
         int n = grid[0].length;
         List<List<Integer>> result = new ArrayList<>(m);
         for (int i = 0; i < m; i++) {
-            List<Integer> list = new ArrayList<Integer>(n);
+            List<Integer> list = new ArrayList<>(n);
             for (int j = 0; j < n; j++) {
-                list.add(grid[i][j]);
+                int shift = m * n - (k % (m * n));
+                int newLinearIndex = (i * n + j + shift) % (m * n);
+                int newI = newLinearIndex / n;
+                int newJ = newLinearIndex % n;
+                list.add(grid[newI][newJ]);
             }
             result.add(list);
         }
