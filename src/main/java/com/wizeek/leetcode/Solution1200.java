4d85dcda63b97e6383f54b2214a3b1f2dde8ba0c
Artur <wizeek80@gmail.com>
2022-05-18 22:51:49 -0700

    1200 count sort (not actually faster)

--- src/main/java/com/wizeek/leetcode/Solution1200.java
+++ src/main/java/com/wizeek/leetcode/Solution1200.java
@@ -1,28 +1,42 @@
 package com.wizeek.leetcode;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
 
 public class Solution1200 {
     public List<List<Integer>> minimumAbsDifference(int[] arr) {
-        Arrays.sort(arr);
-        int minDiff = Integer.MAX_VALUE;
-        for (int i = 0; i < arr.length - 1; i++) {
-            int diff = arr[i + 1] - arr[i];
-            if (minDiff > diff) {
-                minDiff = diff;
-            }
+        int minValue = Integer.MAX_VALUE;
+        int maxValue = Integer.MIN_VALUE;
+        for (int num : arr) {
+            minValue = Math.min(minValue, num);
+            maxValue = Math.max(maxValue, num);
         }
+        int[] values = new int[maxValue - minValue + 1];
+        for (int num : arr) {
+            values[num - minValue]++;
+        }
+        Integer previous = null;
+        Integer current = null;
+        int minDiff = Integer.MAX_VALUE;
         List<List<Integer>> result = new LinkedList<>();
-        for (int i = 0; i < arr.length - 1; i++) {
-            int diff = arr[i + 1] - arr[i];
-            if (diff == minDiff) {
-                List<Integer> pair = new ArrayList<>(2);
-                pair.add(arr[i]);
-                pair.add(arr[i + 1]);
-                result.add(pair);
+        for (int i = 0; i < values.length; i++) {
+            if (values[i] == 0) {
+                continue;
+            }
+            if (current == null) {
+                current = i;
+                continue;
+            }
+            previous = current;
+            current = i;
+            int diff = current - previous;
+            if (diff < minDiff) {
+                minDiff = diff;
+                result.clear();
+                result.add(Arrays.asList(previous + minValue, current + minValue));
+            } else if (diff == minDiff) {
+                result.add(Arrays.asList(previous + minValue, current + minValue));
             }
         }
         return result;
--- src/test/java/com/wizeek/leetcode/Solution1200Test.java
+++ src/test/java/com/wizeek/leetcode/Solution1200Test.java
@@ -3,6 +3,8 @@ package com.wizeek.leetcode;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.List;
+
 import static org.junit.Assert.assertEquals;
 
 public class Solution1200Test {
@@ -16,6 +18,10 @@ public class Solution1200Test {
 
     @Test
     public void test1() {
-        assertEquals(3, solution.minimumAbsDifference(new int[]{4, 2, 1, 3}).size());
+        List<List<Integer>> result = solution.minimumAbsDifference(new int[]{4, 2, 1, 3});
+
+        assertEquals(3, result.size());
+        assertEquals(1, (int) result.get(0).get(0));
+        assertEquals(2, (int) result.get(0).get(1));
     }
 }
