168275f8d9c64f76269d1131eb75e1aa64bf7516
Artur <wizeek80@gmail.com>
2022-05-19 12:44:35 -0700

    67 bitwise operations and BigInteger

--- src/main/java/com/wizeek/leetcode/Solution67.java
+++ src/main/java/com/wizeek/leetcode/Solution67.java
@@ -1,29 +1,20 @@
 package com.wizeek.leetcode;
 
+import java.math.BigInteger;
+
 public class Solution67 {
     public String addBinary(String a, String b) {
-        int n = a.length();
-        int m = b.length();
-        StringBuilder result = new StringBuilder();
-        int carry = 0;
-        int i = 0;
-        while (n - i > 0 || m - i > 0) {
-            int ai = 0;
-            int bi = 0;
-            if (n - i > 0) {
-                ai = a.charAt(n - i - 1) - '0';
-            }
-            if (m - i > 0) {
-                bi = b.charAt(m - i - 1) - '0';
-            }
-            int sum = ai + bi + carry;
-            carry = sum / 2;
-            result.append((char) ('0' + sum % 2));
-            i++;
+        BigInteger aInt = new BigInteger(a, 2);
+        BigInteger bInt = new BigInteger(b, 2);
+        BigInteger zero = new BigInteger("0", 2);
+        BigInteger sum;
+        BigInteger carry;
+        while (!bInt.equals(zero)) {
+            sum = aInt.xor(bInt);
+            carry = aInt.and(bInt).shiftLeft(1);
+            aInt = sum;
+            bInt = carry;
         }
-        if (carry > 0) {
-            result.append('1');
-        }
-        return result.reverse().toString();
+        return aInt.toString(2);
     }
 }
