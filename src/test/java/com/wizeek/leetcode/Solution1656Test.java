0dc0ebe5e39189b20a8c2a959addb4c6419cdee3
Artur <wizeek80@gmail.com>
2022-05-19 19:38:04 -0700

    1656 fix test

--- src/test/java/com/wizeek/leetcode/Solution1656Test.java
+++ src/test/java/com/wizeek/leetcode/Solution1656Test.java
@@ -2,7 +2,7 @@ package com.wizeek.leetcode;
 
 import org.junit.Test;
 
-import java.util.LinkedList;
+import java.util.List;
 
 import static org.junit.Assert.assertEquals;
 
@@ -11,7 +11,10 @@ public class Solution1656Test {
     @Test
     public void test1() {
         Solution1656 stream = new Solution1656(5);
-        assertEquals(new LinkedList<>(), stream.insert(3, "ccccc"));
-        assertEquals(new LinkedList<>(), stream.insert(1, "aaaaa"));
+        assertEquals(List.of(), stream.insert(3, "ccccc"));
+        assertEquals(List.of("aaaaa"), stream.insert(1, "aaaaa"));
+        assertEquals(List.of("bbbbb", "ccccc"), stream.insert(2, "bbbbb"));
+        assertEquals(List.of(), stream.insert(5, "eeeee"));
+        assertEquals(List.of("ddddd", "eeeee"), stream.insert(4, "ddddd"));
     }
 }
