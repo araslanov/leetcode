ba822b8d1be635fd90460dab3cf830458422e1c8
Artur <wizeek80@gmail.com>
2022-05-22 22:00:50 -0700

    716 linked list with storing max

--- src/main/java/com/wizeek/leetcode/Solution716.java
+++ src/main/java/com/wizeek/leetcode/Solution716.java
@@ -4,41 +4,51 @@ import java.util.LinkedList;
 
 public class Solution716 {
     private LinkedList<Integer> stack = new LinkedList<>();
+    private int maxIndex;
+    private int maxValue = Integer.MIN_VALUE;
 
     public Solution716() {
-
     }
 
     public void push(int x) {
-        stack.addFirst(x);
+        stack.addLast(x);
+        if (maxValue <= x) {
+            maxValue = x;
+            maxIndex = stack.size() - 1;
+        }
     }
 
     public int pop() {
-        return stack.pollFirst();
+        int result = stack.pollLast();
+        if (result == maxValue) {
+            updateMax();
+        }
+        return result;
     }
 
     public int top() {
-        return stack.peekFirst();
+        return stack.peekLast();
     }
 
     public int peekMax() {
-        int max = Integer.MIN_VALUE;
-        for (int value : stack) {
-            if (max < value) {
-                max = value;
-            }
-        }
-        return max;
+        return maxValue;
     }
 
     public int popMax() {
-        int max = Integer.MIN_VALUE;
-        for (int value : stack) {
-            if (max < value) {
-                max = value;
-            }
-        }
-        stack.removeFirstOccurrence(max);
+        int max = stack.remove(maxIndex);
+        updateMax();
         return max;
+    }
+
+    private void updateMax() {
+        maxValue = Integer.MIN_VALUE;
+        int i = 0;
+        for (int value : stack) {
+            if (maxValue <= value) {
+                maxValue = value;
+                maxIndex = i;
+            }
+            i++;
+        }
     }
 }
