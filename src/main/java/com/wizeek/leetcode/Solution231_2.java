0c98ebc528c73ad918448b3ef3d2710bc7ed2968
Artur <wizeek80@gmail.com>
2022-05-25 12:50:38 -0700

    231 remove unnecessary check

--- src/main/java/com/wizeek/leetcode/Solution231_2.java
+++ src/main/java/com/wizeek/leetcode/Solution231_2.java
@@ -5,9 +5,6 @@ public class Solution231_2 {
         if (n < 1) {
             return false;
         }
-        if (n == 1) {
-            return true;
-        }
         while (n > 1) {
             if (n % 2 > 0) {
                 return false;
