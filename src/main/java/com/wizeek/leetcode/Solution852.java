eff70709b599e4c443735536f32bae05e0ef139b
Artur <wizeek80@gmail.com>
2022-05-28 00:11:22 -0700

    852 two pointers

--- src/main/java/com/wizeek/leetcode/Solution852.java
+++ src/main/java/com/wizeek/leetcode/Solution852.java
@@ -3,10 +3,15 @@ package com.wizeek.leetcode;
 public class Solution852 {
     public int peakIndexInMountainArray(int[] arr) {
         int n = arr.length;
-        for (int i = 1; i < n; i++) {
+        int i = 1;
+        while (i <= n / 2) {
             if (arr[i] <= arr[i - 1]) {
                 return i - 1;
             }
+            if (arr[n - i - 2] <= arr[n - i - 1]) {
+                return n - i - 1;
+            }
+            i++;
         }
         return -1;
     }
