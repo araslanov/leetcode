8f1ac0c233c1818ab4e99173b22abaf407bad9ea
Artur <wizeek80@gmail.com>
2022-05-30 23:58:39 -0700

    205 two arrays of 256 size

--- src/main/java/com/wizeek/leetcode/Solution205.java
+++ src/main/java/com/wizeek/leetcode/Solution205.java
@@ -1,21 +1,24 @@
 package com.wizeek.leetcode;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.Arrays;
 
 public class Solution205 {
     public boolean isIsomorphic(String s, String t) {
-        Map<Character, Character> map = new HashMap<>();
+        int[] there = new int[256];
+        Arrays.fill(there, -1);
+        int[] back = new int[256];
+        Arrays.fill(back, -1);
         int n = s.length();
         for (int i = 0; i < n; i++) {
             char sc = s.charAt(i);
             char tc = t.charAt(i);
-            Character replace = map.get(sc);
-            if (replace == null) {
-                if (map.containsValue(tc)) {
+            int replace = there[sc];
+            if (replace < 0) {
+                if (back[tc] > 0) {
                     return false;
                 }
-                map.put(sc, tc);
+                there[sc] = tc;
+                back[tc] = sc;
             } else {
                 if (replace != tc) {
                     return false;
