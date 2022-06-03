9cde8f8c6a71e07f63cc89c5877b47382b995388
Artur <wizeek80@gmail.com>
2022-06-03 13:06:59 -0700

    836 simplify conditions

--- src/main/java/com/wizeek/leetcode/Solution836.java
+++ src/main/java/com/wizeek/leetcode/Solution836.java
@@ -2,10 +2,6 @@ package com.wizeek.leetcode;
 
 public class Solution836 {
     public boolean isRectangleOverlap(int[] rec1, int[] rec2) {
-        boolean top = rec2[1] >= rec1[3];
-        boolean right = rec2[0] >= rec1[2];
-        boolean bottom = rec2[3] <= rec1[1];
-        boolean left = rec2[2] <= rec1[0];
-        return !(top || right || bottom || left);
+        return rec2[1] < rec1[3] && rec2[0] < rec1[2] && rec2[3] > rec1[1] && rec2[2] > rec1[0];
     }
 }
