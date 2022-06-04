0eebd03adc421873ee91fe5f379158be669a18e1
Artur <wizeek80@gmail.com>
2022-06-03 20:53:47 -0700

    560 calculating current sum on the go, O(n2) time O(1) memory, time limit exceeded

---
+++ src/main/java/com/wizeek/leetcode/Solution560_2.java
@@ -0,0 +1,18 @@
+package com.wizeek.leetcode;
+
+public class Solution560_2 {
+    public int subarraySum(int[] nums, int k) {
+        int count = 0;
+        int n = nums.length;
+        for (int i = 0; i < n; i++) {
+            int sum = 0;
+            for (int j = i; j < n; j++) {
+                sum += nums[j];
+                if (sum == k) {
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+}
