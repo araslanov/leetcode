25f0ab01f22818d4c2dc1b0538df8d7014956a63
Artur <wizeek80@gmail.com>
2022-06-08 16:23:44 -0700

    921 simplify code

--- src/main/java/com/wizeek/leetcode/Solution921.java
+++ src/main/java/com/wizeek/leetcode/Solution921.java
@@ -4,31 +4,19 @@ public class Solution921 {
     public int minAddToMakeValid(String s) {
         int result = 0;
         int score = 0;
-        if (s.charAt(0) == '(') {
-            score++;
-        } else {
-            result++;
-        }
         int n = s.length();
-        for (int i = 1; i < n; i++) {
+        for (int i = 0; i < n; i++) {
             char c = s.charAt(i);
-            char prev = s.charAt(i - 1);
             if (c == '(') {
-                if (prev == ')') {
-                    score = score > 0 ? score + 1 : 1;
-                } else {
-                    score++;
-                }
+                score++;
             } else {
                 score--;
                 if (score < 0) {
                     result++;
+                    score++;
                 }
             }
         }
-        if (score > 0) {
-            result += score;
-        }
-        return result;
+        return result + score;
     }
 }
