da89cb634168bb5ad47bcc6724c14c6764edb193
Artur <wizeek80@gmail.com>
2022-06-08 15:38:16 -0700

    22 backtracking with string builder

--- src/main/java/com/wizeek/leetcode/Solution22.java
+++ src/main/java/com/wizeek/leetcode/Solution22.java
@@ -6,20 +6,24 @@ import java.util.List;
 public class Solution22 {
     public List<String> generateParenthesis(int n) {
         List<String> result = new LinkedList<>();
-        recurse(result, "(", n - 1, n);
+        recurse(result, new StringBuilder(), n, n);
         return result;
     }
 
-    private void recurse(List<String> result, String base, int openingLeft, int closingLeft) {
+    private void recurse(List<String> result, StringBuilder sb, int openingLeft, int closingLeft) {
         if (closingLeft == 0) {
-            result.add(base);
+            result.add(sb.toString());
             return;
         }
         if (openingLeft > 0) {
-            recurse(result, base + "(", openingLeft - 1, closingLeft);
+            sb.append("(");
+            recurse(result, sb, openingLeft - 1, closingLeft);
+            sb.deleteCharAt(sb.length() - 1);
         }
         if (closingLeft > openingLeft) {
-            recurse(result, base + ")", openingLeft, closingLeft - 1);
+            sb.append(")");
+            recurse(result, sb, openingLeft, closingLeft - 1);
+            sb.deleteCharAt(sb.length() - 1);
         }
     }
 }
