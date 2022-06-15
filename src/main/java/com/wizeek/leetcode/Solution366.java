06f1ec189ebb18029534deaa46e40171feb2be4b
Artur <wizeek80@gmail.com>
2022-06-14 21:28:01 -0700

    366 dfs with array

--- src/main/java/com/wizeek/leetcode/Solution366.java
+++ src/main/java/com/wizeek/leetcode/Solution366.java
@@ -1,29 +1,30 @@
 package com.wizeek.leetcode;
 
-import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
-import java.util.Map;
 
 public class Solution366 {
     public List<List<Integer>> findLeaves(TreeNode root) {
-        Map<Integer, List<Integer>> nodes = new HashMap<>();
+        LinkedList<Integer>[] nodes = new LinkedList[100];
         int max = dfs(nodes, root);
         List<List<Integer>> result = new LinkedList();
         for (int i = 0; i <= max; i++) {
-            result.add(nodes.get(i));
+            result.add(nodes[i]);
         }
         return result;
     }
 
-    private int dfs(Map<Integer, List<Integer>> nodes, TreeNode node) {
+    private int dfs(LinkedList<Integer>[] nodes, TreeNode node) {
         if (node == null) {
             return -1;
         }
         int left = dfs(nodes, node.left);
         int right = dfs(nodes, node.right);
         int current = Math.max(left, right) + 1;
-        nodes.computeIfAbsent(current, key -> new LinkedList<>()).add(node.val);
+        if (nodes[current] == null) {
+            nodes[current] = new LinkedList<>();
+        }
+        nodes[current].add(node.val);
         return current;
     }
 
