5bd6efa974dd8889df29d6c7721d95d5188f94e1
Artur <wizeek80@gmail.com>
2022-06-15 22:46:37 -0700

    1356 custom quick sort

--- src/main/java/com/wizeek/leetcode/Solution1356.java
+++ src/main/java/com/wizeek/leetcode/Solution1356.java
@@ -1,12 +1,42 @@
 package com.wizeek.leetcode;
 
-import java.util.Arrays;
-import java.util.Comparator;
-
 public class Solution1356 {
     public int[] sortByBits(int[] arr) {
-        Integer[] boxed = Arrays.stream(arr).boxed().toArray(Integer[]::new);
-        Arrays.sort(boxed, Comparator.comparingInt(Integer::bitCount).thenComparingInt(a -> a));
-        return Arrays.stream(boxed).mapToInt(Integer::intValue).toArray();
+        quicksort(arr, 0, arr.length - 1);
+        return arr;
+    }
+
+    private void quicksort(int[] arr, int left, int right) {
+        int pivotIndex = partition(arr, left, right);
+        if (pivotIndex > left) {
+            quicksort(arr, left, pivotIndex - 1);
+        }
+        if (pivotIndex < right) {
+            quicksort(arr, pivotIndex + 1, right);
+        }
+    }
+
+    private int partition(int[] arr, int left, int right) {
+        int pivot = arr[left];
+        int prev = right;
+        for (int i = right; i >= left; i--) {
+            if (compare(arr[i], pivot) > 0) {
+                swap(arr, i, prev);
+                prev--;
+            }
+        }
+        swap(arr, left, prev);
+        return prev;
+    }
+
+    private void swap(int[] arr, int left, int right) {
+        int temp = arr[left];
+        arr[left] = arr[right];
+        arr[right] = temp;
+    }
+
+    private int compare(int a, int b) {
+        int bitCompare = Integer.bitCount(a) - Integer.bitCount(b);
+        return bitCompare == 0 ? a - b : bitCompare;
     }
 }
