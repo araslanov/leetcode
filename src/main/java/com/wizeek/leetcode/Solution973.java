445d57e303bb0b8dd8aa52a00ab51d5fe6243303
Artur <wizeek80@gmail.com>
2022-06-19 23:25:50 -0700

    973 check largest

--- src/main/java/com/wizeek/leetcode/Solution973.java
+++ src/main/java/com/wizeek/leetcode/Solution973.java
@@ -4,15 +4,15 @@ import java.util.PriorityQueue;
 
 public class Solution973 {
     public int[][] kClosest(int[][] points, int k) {
-        PriorityQueue<int[]> queue = new PriorityQueue<>((int[] a, int[] b) -> {
-            double left = Math.sqrt(a[0] * a[0] + a[1] * a[1]);
-            double right = Math.sqrt(b[0] * b[0] + b[1] * b[1]);
-            return left < right ? 1 : -1;
-        });
+        PriorityQueue<int[]> queue = new PriorityQueue<>(this::compare);
         for (int[] p : points) {
-            queue.offer(p);
-            if (queue.size() > k) {
-                queue.poll();
+            if (queue.size() < k) {
+                queue.offer(p);
+            } else {
+                if (compare(p, queue.peek()) > 0) {
+                    queue.poll();
+                    queue.offer(p);
+                }
             }
         }
         int[][] result = new int[k][];
@@ -20,5 +20,11 @@ public class Solution973 {
             result[i] = queue.poll();
         }
         return result;
+    }
+
+    private int compare(int[] a, int[] b) {
+        double left = Math.sqrt(a[0] * a[0] + a[1] * a[1]);
+        double right = Math.sqrt(b[0] * b[0] + b[1] * b[1]);
+        return left < right ? 1 : -1;
     }
 }
