49e390cca55289d9dc3bffe914c9b05a8357d29d
Artur <wizeek80@gmail.com>
2022-06-22 02:06:41 -0700

    347 quick select

---
+++ src/main/java/com/wizeek/leetcode/Solution347_2.java
@@ -0,0 +1,61 @@
+package com.wizeek.leetcode;
+
+import java.util.Deque;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Map;
+
+public class Solution347_2 {
+    public int[] topKFrequent(int[] nums, int k) {
+        Map<Integer, Integer> count = new HashMap<>();
+        for (int num : nums) {
+            count.put(num, count.getOrDefault(num, 0) + 1);
+        }
+        Map<Integer, Deque<Integer>> reverse = new HashMap<>();
+        int c = count.size();
+        int[] reps = new int[c];
+        int i = 0;
+        for (Map.Entry<Integer, Integer> entry : count.entrySet()) {
+            reps[i] = entry.getValue();
+            reverse.computeIfAbsent(reps[i], key -> new LinkedList<>()).offerLast(entry.getKey());
+            i++;
+        }
+        quickSelect(reps, 0, c - 1, c - k);
+        int[] result = new int[k];
+        int j = 0;
+        for (i = c - k; i < c; i++) {
+            result[j++] = reverse.get(reps[i]).pollLast();
+        }
+        return result;
+    }
+
+    private void quickSelect(int[] reps, int left, int right, int k) {
+        int pivot = partition(reps, left, right);
+        if (pivot == k) {
+            return;
+        } else if (pivot > k) {
+            quickSelect(reps, left, pivot - 1, k);
+        } else {
+            quickSelect(reps, pivot + 1, right, k);
+        }
+    }
+
+    private int partition(int[] reps, int left, int right) {
+        int pivot = reps[left];
+        int prev = right;
+        for (int i = right; i >= left; i--) {
+            if (reps[i] > pivot) {
+                swap(reps, i, prev);
+                prev--;
+            }
+        }
+        swap(reps, left, prev);
+        return prev;
+    }
+
+    private void swap(int[] reps, int i, int j) {
+        int temp = reps[i];
+        reps[i] = reps[j];
+        reps[j] = temp;
+    }
+}
---
+++ src/test/java/com/wizeek/leetcode/Solution347_2Test.java
@@ -0,0 +1,37 @@
+package com.wizeek.leetcode;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertArrayEquals;
+
+public class Solution347_2Test {
+
+    private Solution347_2 solution;
+
+    @Before
+    public void setUp() {
+        solution = new Solution347_2();
+    }
+
+    @Test
+    public void test1() {
+        assertArrayEquals(new int[]{2, 1}, solution.topKFrequent(new int[]{1, 1, 1, 2, 2, 3}, 2));
+    }
+
+    @Test
+    public void test2() {
+        assertArrayEquals(new int[]{1}, solution.topKFrequent(new int[]{1}, 1));
+    }
+
+    @Test
+    public void test3() {
+        assertArrayEquals(new int[]{2, 1}, solution.topKFrequent(new int[]{1, 2}, 2));
+    }
+
+    @Test
+    public void test4() {
+        assertArrayEquals(new int[]{11, 10, 5, 7, 8, 2, 6, 3, 4, 1}, solution.topKFrequent(
+                new int[]{3, 2, 3, 1, 2, 4, 5, 5, 6, 7, 7, 8, 2, 3, 1, 1, 1, 10, 11, 5, 6, 2, 4, 7, 8, 5, 6}, 10));
+    }
+}
